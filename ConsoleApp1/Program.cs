31c71dd88488646fa4f192ff75ab24956bd57183
gorsk <gorsk@LAPTOP-D3HKAM1F>
2024-02-06 23:15:06 +0100

    Praca domowa lekcja 4

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "EWA";
+var age = 30;
+var sex = "KOBIETA";
+
+if (age < 30 && sex == "KOBIETA")
+{
+    Console.WriteLine("KOBIETA PONIZEJ 30 LAT.");
+}
+else if (name == "EWA" && age == 30)
+{
+    Console.WriteLine("EWA, LAT 30");
+}
+else if (!(sex == "KOBIETA") && age < 18)
+{
+   Console.WriteLine("NIEPELNOLETNI MEZCZYZNA");
+}
+else if (!(sex == "KOBIETA") && age > 18)
+{
+    Console.WriteLine(" ");
+}
+
